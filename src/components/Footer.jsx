747ef7599ace2012f343a0f7ea278a2d76d85e85
DantechJr <dantecalcantarajr@gmail.com>
2023-08-11 14:20:50 +0800

    footer layout fixed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,11 +5,14 @@ const Footer = () => {
   return (
     <>
       <footer>
-        <div className="row p-3  justify-content-between mx-auto">
-          <div className="col-sm-12 col-md-6">
+        <div className="row p-3 d-flex  justify-content-between mx-auto">
+          <div className="col-sm-12 col-md-6" style={{ width: "200px" }}>
             <small>&copy; 2023 Dante.Alcantara.Jr™</small>
           </div>
-          <div className="col-sm-12 col-md-6 justify-content-end social-icon d-flex">
+          <div
+            className="col-sm-12 col-md-6 justify-content-end social-icon d-flex"
+            style={{ width: "100px" }}
+          >
             <li className="ms-3">
               <a className="text-body-secondary" href="#">
                 <i className="bi bi-github" width="24" height="24"></i>
